e979dac499c0835cc44e1c992bf06ce5cfa901ed
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2023-10-31 00:51:35 +0800

    Misc: update copyright notice

--- Source/RemWidgetComponent/Private/RemWidgetComponentStatics.cpp
+++ Source/RemWidgetComponent/Private/RemWidgetComponentStatics.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright RemRemRemRe, All Rights Reserved.
 
 
 #include "RemWidgetComponentStatics.h"
--- Source/RemWidgetComponent/Public/RemWidgetComponentLog.h
+++ Source/RemWidgetComponent/Public/RemWidgetComponentLog.h
@@ -1,4 +1,4 @@
-﻿// Copyright . All Rights Reserved.
+﻿// Copyright RemRemRemRe, All Rights Reserved.
 
 #pragma once
 
