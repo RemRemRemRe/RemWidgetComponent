afcbbb31e8a2f3e6e5110533846ea37fc4dda94a
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2024-12-14 23:31:06 +0800

    Misc: using UnrealBuildTool

--- Source/RemWidgetComponent/RemWidgetComponent.Build.cs
+++ Source/RemWidgetComponent/RemWidgetComponent.Build.cs
@@ -1,33 +1,31 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright RemRemRemRe. All Rights Reserved.
 
+using UnrealBuildTool;
 
-namespace UnrealBuildTool.Rules
+public class RemWidgetComponent : ModuleRules
 {
-	public class RemWidgetComponent : ModuleRules
+	public RemWidgetComponent(ReadOnlyTargetRules target) : base(target)
 	{
-		public RemWidgetComponent(ReadOnlyTargetRules target) : base(target)
-		{
-			ShadowVariableWarningLevel = WarningLevel.Error;
-			IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
-			DefaultBuildSettings = BuildSettingsVersion.Latest;
-			CppStandard = CppStandardVersion.EngineDefault;
-			UnsafeTypeCastWarningLevel = WarningLevel.Warning;
-
-			bEnableNonInlinedGenCppWarnings = true;
-			bUseUnity = false;
-
-			PrivateDependencyModuleNames.AddRange(
-				new[]
-				{
-					"Core",
-					"CoreUObject",
-					"Engine",
-
-					"UMG",
-					
-					"RemCommon",
-				}
-			);
-		}
+		ShadowVariableWarningLevel = WarningLevel.Error;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		CppStandard = CppStandardVersion.EngineDefault;
+		UnsafeTypeCastWarningLevel = WarningLevel.Warning;
+		
+		bEnableNonInlinedGenCppWarnings = true;
+		bUseUnity = false;
+		
+		PrivateDependencyModuleNames.AddRange(
+			new[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine",
+				
+				"UMG",
+				
+				"RemCommon",
+			}
+		);
 	}
 }
