aa490622119861ef77a28ddb8d749df01d86d0dd
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2025-09-12 02:19:46 +0800

    Changed: adopt REM_API style

--- Source/RemWidgetComponent/Public/RemWidgetComponentLog.h
+++ Source/RemWidgetComponent/Public/RemWidgetComponentLog.h
@@ -3,5 +3,10 @@
 #pragma once
 
 #include "Logging/LogMacros.h"
+#include "Macro/RemMacroUtilities.h"
 
-REMWIDGETCOMPONENT_API DECLARE_LOG_CATEGORY_EXTERN(LogRemWidgetComponent, Log, All);
+#define REM_API REMWIDGETCOMPONENT_API
+
+REM_API DECLARE_LOG_CATEGORY_EXTERN(LogRemWidgetComponent, REM_DEFAULT_LOG_VERBOSITY, REM_MAX_LOG_VERBOSITY);
+
+#undef REM_API
