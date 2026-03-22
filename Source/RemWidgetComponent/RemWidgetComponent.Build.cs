e5567db3ac4129b8a61a0ed843f7e392b7543f68
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2026-03-23 00:50:09 +0800

    New: introduce RemSharedModuleRules to avoid duplication work

--- Source/RemWidgetComponent/RemWidgetComponent.Build.cs
+++ Source/RemWidgetComponent/RemWidgetComponent.Build.cs
@@ -1,19 +1,13 @@
 // Copyright RemRemRemRe. All Rights Reserved.
 
 using UnrealBuildTool;
+using Rem.BuildRule;
 
 public class RemWidgetComponent : ModuleRules
 {
 	public RemWidgetComponent(ReadOnlyTargetRules target) : base(target)
 	{
-		CppCompileWarningSettings.ShadowVariableWarningLevel = WarningLevel.Error;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
-		DefaultBuildSettings = BuildSettingsVersion.Latest;
-		CppStandard = CppStandardVersion.EngineDefault;
-		CppCompileWarningSettings.UnsafeTypeCastWarningLevel = WarningLevel.Warning;
-		
-		CppCompileWarningSettings.NonInlinedGenCppWarningLevel = WarningLevel.Warning;
-		bUseUnity = false;
+        RemSharedModuleRules.Apply(this);
 		
 		PrivateDependencyModuleNames.AddRange(
 			[
